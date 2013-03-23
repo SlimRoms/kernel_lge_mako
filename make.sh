d703a71ddf6fc8eea996a1dead3a9ee305139603
Josue Rivera <prbassplayer@gmail.com>
2013-03-23 15:54:38 -0400

    Stupid bash to make faster for me

---
+++ make.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+export ARCH=arm
+echo 'exporting Arch'
+export SUBARCH=arm
+echo 'exporting SubArch'
+export CROSS_COMPILE=/home/josh/Android/toolchain/4.6/bin/arm-linux-androideabi-
+echo 'exporting Cross Compile'
+echo 'Cleaning build'
+make clean
+echo 'Making defconfig for Mako'
+make mako_defconfig
+echo 'Lets start!'
+make -j4
