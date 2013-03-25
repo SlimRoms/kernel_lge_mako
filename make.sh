8eaa61ab7437faca292692795ff197780e298ca0
Josue Rivera <prbassplayer@gmail.com>
2013-03-24 20:41:22 -0400

    Modifid the sh file

--- make.sh
+++ make.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 export ARCH=arm
 echo 'exporting Arch'
 export SUBARCH=arm
@@ -8,7 +9,9 @@ export CROSS_COMPILE=/home/josh/Android/toolchain/4.6/bin/arm-linux-androideabi-
 echo 'exporting Cross Compile'
 echo 'Cleaning build'
 make clean
+if [ "$2" = "config" ] ; then
 echo 'Making defconfig for Mako'
 make mako_defconfig
+fi
 echo 'Lets start!'
-make -j4
+make -j$1
