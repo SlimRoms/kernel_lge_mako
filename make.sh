89eb980339a1f2872f0692958d2bfdeca085cd2c
Josue Rivera <prbassplayer@gmail.com>
2013-03-26 13:52:18 -0400

    Modified bash script

--- make.sh
+++ make.sh
@@ -9,9 +9,10 @@ export CROSS_COMPILE=/home/josh/Android/toolchain/4.6/bin/arm-linux-androideabi-
 echo 'exporting Cross Compile'
 echo 'Cleaning build'
 make clean
-if [ "$2" = "config" ] ; then
+if [ "$1" = "config" ] ; then
 echo 'Making defconfig for Mako'
 make mako_defconfig
+exit
 fi
 echo 'Lets start!'
 make -j$1
