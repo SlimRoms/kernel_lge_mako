0fcf34d549cd768748734eae68c0b443d2807416
Josue Rivera <prbassplayer@gmail.com>
2013-11-20 06:04:18 +0100

    Start  new defconfig and added build script
    Change-Id: I60afee1d1b70146a4598b15f703daa1615519db3
    Signed-off-by: Josue Rivera <prbassplayer@gmail.com>

---
+++ make.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Stupid shell script to compile kernel, nothing fancy
+
+# Exports all the needed things Arch, SubArch and Cross Compile
+export ARCH=arm
+echo 'exporting Arch'
+export SUBARCH=arm
+echo 'exporting SubArch'
+export CROSS_COMPILE=/home/prbassplayer/WIP4.3/prebuilt/linux-x86/toolchain/linaro/bin/arm-linux-gnueabihf-
+#export CROSS_COMPILE=/home/prbassplayer/WIP/prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin/arm-eabi-
+echo 'exporting Cross Compile'
+
+# Make sure build is clean!
+echo 'Cleaning build'
+make clean
+
+# Generates a new .config and exists
+if [ "$1" = "config" ] ; then
+echo 'Making defconfig for Mako'
+make slim_mako_defconfig
+exit
+fi
+
+# Exports kernel local version? Not sure yet.
+#echo 'Exporting kernel version'
+#export LOCALVERSION='SlimTest_1.0'
+
+# Lets go!
+echo 'Lets start!'
+make -j$1
