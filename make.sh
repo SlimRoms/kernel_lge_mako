6f0345327989348ae0cc02691f2d3e90a7889e4b
Josue Rivera <prbassplayer@gmail.com>
2014-11-30 21:40:12 +0100

    Mako: Add script
    Change-Id: I7dc3ad18bf9b17d81862a7089da3d0382677feb9
    Signed-off-by: Josue Rivera <prbassplayer@gmail.com>

---
+++ make.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Stupid shell script to compile kernel, nothing fancy
+
+# Exports all the needed things Arch, SubArch and Cross Compile
+export ARCH=arm
+echo 'exporting Arch'
+export SUBARCH=arm
+echo 'exporting SubArch'
+#export CROSS_COMPILE=/home/prbassplayer/lp5.0-aosp/prebuilt/linux-x86/toolchain/linaro-4.8-13.06/bin/arm-linux-gnueabihf-
+export CROSS_COMPILE=/home/prbassplayer/lp5.0-aosp/prebuilts/gcc/linux-x86/arm/arm-eabi-4.8/bin/arm-eabi-
+echo 'exporting Cross Compile'
+
+# Make sure build is clean!
+echo 'Cleaning build'
+make clean
+
+# Generates a new .config and exists
+if [ "$1" = "config" ] ; then
+echo 'Making defconfig for Mako'
+make slim_mako_defconfig
+exit
+fi
+
+# Exports kernel local version? Not sure yet.
+#echo 'Exporting kernel version'
+#export LOCALVERSION='SlimTest_1.0'
+
+# Lets go!
+echo 'Lets start!'
+make -j$1
